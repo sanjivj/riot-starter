404e40fa81e09b2d6f7e03e87724b9b11f99a4e6
Gilbert <gilbertbgarza@gmail.com>
2013-12-18 06:47:22 -0600

    Function wrappers, convert tabs to spaces

--- js/list-item.js
+++ js/list-item.js
@@ -1,26 +1,30 @@
-var ListItem = function () {
-	// initialize a checkpoints array for all them to
-	// be stored in
-	window.listItems = [];
-	// make this object watch itself
-	$.observable(this);
+(function () {
 
-	this.create = function (listItemAttributes) {
-		listItems.push(listItemAttributes);
-		// this tells the presenter - Hey! This add thing just happened in the model
-		this.trigger("create", listItemAttributes);
-	};
+  window.ListItem = function () {
+    // initialize a checkpoints array for all them to
+    // be stored in
+    window.listItems = [];
+    // make this object watch itself
+    $.observable(this);
 
-	this.destroy = function (index) {
-		var listItem = listItems.slice(index, (index + 1));
-		this.trigger("destroy", listItem);
-	};
+    this.create = function (listItemAttributes) {
+      listItems.push(listItemAttributes);
+      // this tells the presenter - Hey! This add thing just happened in the model
+      this.trigger("create", listItemAttributes);
+    };
 
-	this.update = function (index, name, priority) {
-		// console.log('index:', index, 'name:', name, 'priority:', priority);
-		listItems[index].name = name;
-		listItems[index].priority = priority;
-		this.trigger("update", listItems[index]);
-	};
+    this.destroy = function (index) {
+      var listItem = listItems.slice(index, (index + 1));
+      this.trigger("destroy", listItem);
+    };
 
-};
+    this.update = function (index, name, priority) {
+      // console.log('index:', index, 'name:', name, 'priority:', priority);
+      listItems[index].name = name;
+      listItems[index].priority = priority;
+      this.trigger("update", listItems[index]);
+    };
+
+  };
+
+})();
